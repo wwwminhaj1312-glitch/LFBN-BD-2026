c720878ed742765656669029627b70f7ab2e7fff
wwwminhaj1312-glitch <www.minhaj1312@gmail.com>
2026-06-28 01:07:05 +0600

    Update location.js

--- pages/location.js
+++ pages/location.js
@@ -1,1 +1,27 @@
+// ===============================
+// LFBN-BD 2026 Location System
+// Part 1 - Divisions
+// ===============================
 
+const locationData = {
+  "চট্টগ্রাম": {},
+  "ঢাকা": {},
+  "রাজশাহী": {},
+  "খুলনা": {},
+  "বরিশাল": {},
+  "সিলেট": {},
+  "রংপুর": {},
+  "ময়মনসিংহ": {}
+};
+
+const divisionSelect = document.getElementById("division");
+const districtSelect = document.getElementById("district");
+const upazilaSelect = document.getElementById("upazila");
+
+// Division Load
+for (const division in locationData) {
+    const option = document.createElement("option");
+    option.value = division;
+    option.textContent = division;
+    divisionSelect.appendChild(option);
+}
