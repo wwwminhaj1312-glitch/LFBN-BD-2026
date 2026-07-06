1e41df21add91e5af8165025a99d0573493c7fde
wwwminhaj1312-glitch <www.minhaj1312@gmail.com>
2026-07-06 22:16:59 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("LFBN-BD 2026 App Started");
+// ===== SAMPLE DONOR DATA =====
+
+const donors = [
+
+{
+    name: "Sample Donor",
+    blood: "O+",
+    district: "Chattogram",
+    phone: "01700000000",
+    status: "Available"
+}
+
+];
